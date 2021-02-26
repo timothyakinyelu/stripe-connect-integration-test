9fe368e7c81ca9c52d620d5620f07770e0beee08
loverofMush <tolujamal1@gmail.com>
2021-02-26 02:36:45 +0100

    - added instance folder

---
+++ instance/config.py
@@ -0,0 +1,23 @@
+import os
+
+
+class BaseConfig:
+    """ Base configuration class"""
+    
+    DEBUG=os.environ.get('DEBUG')
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    WTF_CSRF_ENABLED=True
+
+
+class DevelopmentConfig(BaseConfig):
+    """ Development environment configurations"""
+    
+    TEMPLATES_AUTO_RELOAD=True
+    STRIPE_API_KEY=os.environ.get('STRIPE_API_KEY')
+    STRIPE_SECRET_KEY=os.environ.get('STRIPE_SECRET_KEY')
+    
+    
+
+app_config = {
+    'development': DevelopmentConfig
+}
