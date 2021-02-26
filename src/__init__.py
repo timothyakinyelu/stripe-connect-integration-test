849f1b085bcb414536d9bcf6e035eb84d98a4ed4
loverofMush <tolujamal1@gmail.com>
2021-02-26 02:44:28 +0100

    - connected app to config - setup index view

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from src.helpers.load_config import loadConfig
+from flask_wtf.csrf import CSRFProtect
 
 
+csrf = CSRFProtect()
 def createApp():
     app = Flask(__name__, instance_relative_config=True)
+    mode = app.env
+    Config = loadConfig(mode)
+    app.config.from_object(Config)
     
+    csrf.init_app(app)
+    
+    @app.route('/')
+    def index():
+        return 'Hello World!'
     
     with app.app_context():
         return app
